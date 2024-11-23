8092e3b9c325d1cc5796fa7034151c81c7a4bc12
dcodeciaa <darmawati5354d@gmail.com>
2024-11-23 20:02:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".main").style.opacity = "1";
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".main").style.opacity = "1";
+
+    const skillItems = document.querySelector(".skill-items");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target); // Stop observing once it's shown
+        }
+      });
+    }, {
+      threshold: 0.1 // Trigger when 10% of the item is visible
+    });
+
+    observer.observe(skillItems);
+  }); 
+  
+  
